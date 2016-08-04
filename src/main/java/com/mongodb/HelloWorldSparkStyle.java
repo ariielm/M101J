2f431a718be2168e7a6df5d89f5809aba6a43e4d
Ariel Molina <arielhenrique3@gmail.com>
2016-08-04 08:23:59 -0700

    first commit

---
+++ src/main/java/com/mongodb/HelloWorldSparkStyle.java
@@ -0,0 +1,21 @@
+package com.mongodb;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
+/**
+ * Created by ariielm on 6/7/16.
+ */
+public class HelloWorldSparkStyle {
+
+    public static void main(String[] args) {
+        Spark.get("/", new Route() {
+            public Object handle(Request request, Response response) {
+                return "teste";
+            }
+        });
+    }
+
+}
