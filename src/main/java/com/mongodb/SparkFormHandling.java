6cc048d11275773624e6934899989167611d4304
Ariel Molina <arielhenrique3@gmail.com>
2016-08-05 08:56:28 -0700

    Week 1: Introduction - Done

---
+++ src/main/java/com/mongodb/SparkFormHandling.java
@@ -0,0 +1,56 @@
+package com.mongodb;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
+import java.io.StringWriter;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by ariielm on 8/5/16.
+ */
+public class SparkFormHandling {
+
+    public static void main(String[] args) {
+
+        final Configuration configuration = new Configuration();
+        configuration.setClassForTemplateLoading(SparkFormHandling.class, "/");
+
+        Spark.get("/", new Route() {
+            public Object handle(Request request, Response response) {
+                Map<String, Object> fruitsMap = new HashMap<String, Object>();
+                fruitsMap.put("fruits", Arrays.asList("apple", "orange", "banana", "peach"));
+
+                try {
+                    Template fruitPickerTemplate = configuration.getTemplate("fruitPicker.ftl");
+                    StringWriter writer = new StringWriter();
+                    fruitPickerTemplate.process(fruitsMap, writer);
+                    return writer;
+                } catch (Exception e) {
+                    Spark.halt(500);
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+        });
+
+        Spark.post("/favorite_fruit", new Route() {
+            public Object handle(Request request, Response response) {
+                final String fruit = request.queryParams("fruit");
+                if (fruit == null) {
+                    return "Why don't you pick one?";
+                } else {
+                    return "Your favorite fruit is " + fruit;
+                }
+            }
+        });
+
+    }
+
+}
---
+++ src/main/java/com/mongodb/SparkRoute.java
@@ -0,0 +1,32 @@
+package com.mongodb;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
+/**
+ * Created by ariielm on 8/5/16.
+ */
+public class SparkRoute {
+
+    public static void main(String[] args) {
+        Spark.get("/", new Route() {
+            public Object handle(Request request, Response response) {
+                return "Hello World\n";
+            }
+        });
+
+        Spark.get("/test", new Route() {
+            public Object handle(Request request, Response response) {
+                return "This is a page test\n";
+            }
+        });
+
+        Spark.get("/echo/:thing", new Route() {
+            public Object handle(Request request, Response response) {
+                return request.params("thing");
+            }
+        });
+    }
+}
