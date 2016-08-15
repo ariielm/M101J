92aae2a4d0a7fda985ba7165b1b653ea87fc19fc
Ariel Molina <arielhenrique3@gmail.com>
2016-08-15 07:27:33 -0700

    Week 2: CRUD - Java Driver: Insert

--- src/main/java/com/mongodb/App.java
+++ src/main/java/com/mongodb/App.java
@@ -1,13 +1,20 @@
 package com.mongodb;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.BsonDocument;
+
+public class App  {
+
+    public static void main( String[] args ) {
+        //MongoClient client = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
+
+        MongoClientOptions options = MongoClientOptions.builder().connectionsPerHost(100).build();
+        MongoClient client = new MongoClient(new ServerAddress(), options);
+
+        MongoDatabase db = client.getDatabase("test").withReadPreference(ReadPreference.secondary());
+
+        MongoCollection<BsonDocument> coll = db.getCollection("test", BsonDocument.class);
     }
+
 }
